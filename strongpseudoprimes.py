478c70a4431f9fd1d061235e07ff6c9f30d128d5
excf28 <deee4444@gmail.com>
2024-12-08 17:32:54 +0100

    Command line arguments and progress bar

--- strongpseudoprimes.py
+++ strongpseudoprimes.py
@@ -1,4 +1,16 @@
+import argparse
 import math
+import time
+
+
+def print_progressbar(segment, start_time, resolution):
+    """
+    Prints a progress bar, unrelated to strong pseudoprimes or primality testing
+    """
+    progress = segment // (resolution // 20)
+    print(
+        f"\r[{'=' * progress}{' ' * (20 - progress)}]  {((segment / resolution) * 100):.2f}% {time.time() - start_time:.2f}s",
+        end="")
 
 
 def is_strong_probable_prime(n, a):
@@ -57,7 +69,7 @@ def is_prime(n, skip_base=None):
         d >>= 1
         s += 1
 
-    for a in range(2, min(n - 1, math.floor(2*(math.log(n) ** 2)))):
+    for a in range(2, min(n - 1, math.floor(2 * (math.log(n) ** 2)))):
         if a == skip_base:
             continue
 
@@ -74,6 +86,7 @@ def is_prime(n, skip_base=None):
 
     return True
 
+
 def is_strong_pseudoprime(n, a):
     # 1. check if n is a strong probable prime with the Miller-Rabin test
     if is_strong_probable_prime(n, a):
@@ -81,10 +94,41 @@ def is_strong_pseudoprime(n, a):
         return not is_prime(n, a)
     return False
 
-def search_for_strong_pseudoprimes(base, count):
-    for n in range(3, count, 2):
-        if is_strong_pseudoprime(n, base):
-            print(f"{n} is a strong pseudoprime to base {base}", end="\n\r")
 
-# Find all strong pseudoprimes to base 2 below 100 000
-search_for_strong_pseudoprimes(2, 100000)
+def search_for_strong_pseudoprimes(base, bound, progress_resolution):
+    """
+    Searches for strong pseudoprimes for a given base below a given bound.
+    :param base: The base to search
+    :param bound: The upper bound, essentially the amount of numbers to check (only odd numbers larger than 2)
+    :param progress_resolution: The resolution of the progress bar
+    :return:
+    """
+    print(f"{'-' * 60}")
+    print(f"Searching for strong pseudoprimes to base {base} below {bound}")
+    print(f"{'-' * 60}")
+    start_time = time.time()
+    segment_size = bound / progress_resolution
+    current_segment = 1
+    strong_pseudoprimes = []
+    for n in range(3, bound, 2):
+        if is_strong_pseudoprime(n, base):
+            print(f"\r\33[K{n}\n", end="")
+            strong_pseudoprimes.append(n)
+            print_progressbar(current_segment, start_time, progress_resolution)
+        elif n > (current_segment - 1) * segment_size:
+            print_progressbar(current_segment, start_time, progress_resolution)
+            current_segment += 1
+    print(f"\r\33[K{'-' * 60}")
+    print(f"Found {len(strong_pseudoprimes)} strong pseudoprimes to base {base} below {bound} in {time.time() - start_time:.2f}s")
+    return strong_pseudoprimes
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--base', type=int, help='The base to search for strong pseudoprimes', default=2)
+    parser.add_argument('-c', '--count', type=int, help='The upper bound to search for strong pseudoprimes, essentially the amount of numbers to check',
+                        default=100000)
+    parser.add_argument('-r', '--resolution', type=int, help='The resolution of the progress bar', default=10000)
+    args = parser.parse_args()
+
+    search_for_strong_pseudoprimes(max(args.base, 2), max(args.count, 5), max(args.resolution, 20))
